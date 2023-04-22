594bb9e2c916cc943cb299c43445ecb031a99bc3
alexsanderbeserra <131381518+alexsanderbeserra@users.noreply.github.com>
2023-04-22 00:29:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -94,4 +94,74 @@ function loadClient() {
     });
 }
 
-// Restante do código referente ao YouTube e tabela
+gapi.load("client", loadClient);
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player("player", {
+    height: "360",
+    width: "640",
+    videoId: YOUTUBE_VIDEO_ID,
+  });
+}
+
+(function () {
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+})();
+
+const tbody = document.querySelector("tbody");
+for (let i = 1; i <= 24; i++) {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>Modelo ${i}</td>
+    <td><input type="number" min="0" name="modelo${i}-cor1" id="modelo${i}-cor1"></td>
+    <td><input type="number" min="0" name="modelo${i}-cor2" id="modelo${i}-cor2"></td>
+    <td><input type="number" min="0" name="modelo${i}-cor3" id="modelo${i}-cor3"></td>
+    <td><input type="number" min="0" name="modelo${i}-cor4" id="modelo${i}-cor4"></td>
+`;
+  tbody.appendChild(tr);
+}
+
+document
+  .getElementById("product-selection-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const storeName = document.getElementById("store-name").value;
+    const values = [[storeName]];
+
+    for (let i = 1; i <= 24; i++) {
+      const row = [`Modelo ${i}`];
+      for (let j = 1; j <= 4; j++) {
+        const input = document.getElementById(`modelo${i}-cor${j}`);
+        row.push(input.value || "0");
+      }
+      values.push(row);
+    }
+
+    const sheetsAPI = gapi.client.sheets.spreadsheets.values;
+    sheetsAPI
+      .append({
+        spreadsheetId: SPREADSHEET_ID,
+        range: "Página1",
+        valueInputOption: "RAW",
+        insertDataOption: "INSERT_ROWS",
+        resource: {
+          values: values,
+        },
+      })
+      .then(
+        (response) => {
+          console.log("Resposta da API do Google Sheets:", response.result);
+          alert("Resposta enviada com sucesso!");
+        },
+        (error) => {
+          console.error(
+            "Erro ao enviar dados para o Google Sheets:",
+            error.result.error
+          );
+          alert("Erro ao enviar a resposta. Por favor, tente novamente.");
+        }
+      );
+  });
