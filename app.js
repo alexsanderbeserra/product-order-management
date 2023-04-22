7d4bc5be287685941d9288bbd4c6098450e89079
alexsanderbeserra <131381518+alexsanderbeserra@users.noreply.github.com>
2023-04-22 00:17:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const clientId =
-  "542180890854-cplqpn895bjrb999tl72glk693al392h.apps.googleusercontent.com";
+const clientId = "542180890854-cplqpn895bjrb999tl72glk693al392h.apps.googleusercontent.com";
 const apiKey = "a4ac0722b86b4efac11af8a7b519d461eeb5ef22";
 const scopes = "https://www.googleapis.com/auth/spreadsheets";
 const SPREADSHEET_ID = "1HSwR1dCqCxGl_exE7BK3DY1cFMCsCfwnmsedIfry9cw";
@@ -19,10 +18,7 @@ function initClient() {
     .then(function () {
       gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
       updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      document.getElementById("authorize_button").onclick = handleAuthClick;
-      document.getElementById("signout_button").onclick = handleSignoutClick;
 
-      // Adicione esta linha para carregar a API do Google Sheets após a inicialização do cliente de autenticação
       loadClient();
     });
 }
@@ -51,44 +47,51 @@ function loadClient() {
   gapi.client.setApiKey(apiKey);
   return gapi.client
     .load("https://sheets.googleapis.com/$discovery/rest?version=v4")
-    .then(
-      () => {
-        console.log("GAPI client loaded for API");
+    .then(() => {
+      console.log("GAPI client loaded for API");
 
-        // Mova o código que adiciona o evento de envio do formulário para dentro desta função
-        document
-          .getElementById("product-selection-form")
-          .addEventListener("submit", (event) => {
-            event.preventDefault();
-            const storeName = document.getElementById("store-name").value;
-            const values = [[storeName]];
+      document
+        .getElementById("product-selection-form")
+        .addEventListener("submit", (event) => {
+          event.preventDefault();
+          const storeName = document.getElementById("store-name").value;
+          const values = [[storeName]];
 
-            for (let i = 1; i <= 24; i++) {
-              const row = [`Modelo ${i}`];
-              for (let j = 1; j <= 4; j++) {
-                const input = document.getElementById(`modelo${i}-cor${j}`);
-                row.push(input.value || "0");
-              }
-              values.push(row);
+          for (let i = 1; i <= 24; i++) {
+            const row = [`Modelo ${i}`];
+            for (let j = 1; j <= 4; j++) {
+              const input = document.getElementById(`modelo${i}-cor${j}`);
+              row.push(input.value || "0");
             }
+            values.push(row);
+          }
 
-            const sheetsAPI = gapi.client.sheets.spreadsheets.values;
-            sheetsAPI
-              .append({
-                spreadsheetId: SPREADSHEET_ID,
-                range: "Página1",
-                valueInputOption: "RAW",
-                insertDataOption: "INSERT_ROWS",
-                resource: {
-                  values: values,
-                },
-              })
-              .then(
-                (response) => {
-                  console.log("Resposta da API do Google Sheets:", response.result);
-                  alert("Resposta enviada com sucesso!");
-                },
-                (error) => {
-                  console.error(
-                    "Erro ao enviar dados para o Google Sheets:",
-                    error.result.error
+          const sheetsAPI = gapi.client.sheets.spreadsheets.values;
+          sheetsAPI
+            .append({
+              spreadsheetId: SPREADSHEET_ID,
+              range: "Página1",
+              valueInputOption: "RAW",
+              insertDataOption: "INSERT_ROWS",
+              resource: {
+                values: values,
+              },
+            })
+            .then(
+              (response) => {
+                console.log("Resposta da API do Google Sheets:", response.result);
+                alert("Resposta enviada com sucesso!");
+              },
+              (error) => {
+                console.error(
+                  "Erro ao enviar dados para o Google Sheets:",
+                  error.result.error
+                );
+                alert("Erro ao enviar a resposta. Por favor, tente novamente.");
+              }
+            );
+        });
+    });
+}
+
+// Restante do código referente ao YouTube e tabela
