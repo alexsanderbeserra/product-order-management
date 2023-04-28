07516b1e3bb865a7e170bb1e526b2e3287680672
alexsanderbeserra <131381518+alexsanderbeserra@users.noreply.github.com>
2023-04-28 18:19:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const clientId = "542180890854-cplqpn895bjrb999tl72glk693al392h.apps.googleusercontent.com";
+const clientId =
+  "542180890854-cplqpn895bjrb999tl72glk693al392h.apps.googleusercontent.com";
 const scopes = "https://www.googleapis.com/auth/spreadsheets";
 const SPREADSHEET_ID = "1HSwR1dCqCxGl_exE7BK3DY1cFMCsCfwnmsedIfry9cw";
 const YOUTUBE_VIDEO_ID = "-6dSUf8wAHM";
@@ -21,6 +22,10 @@ function initClient() {
       updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
       document.getElementById("authorize_button").onclick = handleAuthClick;
       document.getElementById("signout_button").onclick = handleSignoutClick;
+
+      gapi.client.setToken(
+        gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse()
+      );
     });
 }
 
@@ -28,7 +33,6 @@ function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     document.getElementById("authorize_button").style.display = "none";
     document.getElementById("signout_button").style.display = "block";
-    submitForm();
   } else {
     document.getElementById("authorize_button").style.display = "block";
     document.getElementById("signout_button").style.display = "none";
@@ -66,52 +70,45 @@ for (let i = 1; i <= 24; i++) {
   tbody.appendChild(tr);
 }
 
-function submitForm() {
-  document
-    .getElementById("product-selection-form")
-    .addEventListener("submit", (event) => {
-      event.preventDefault();
-      const storeName = document.getElementById("store-name").value;
-      const values = [[storeName]];
+document
+  .getElementById("product-selection-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const storeName = document.getElementById("store-name").value;
+    const values = [[storeName]];
 
-      for (let i = 1; i <= 24; i++) {
-        const row = [`Modelo ${i}`];
-        for (let j = 1; j <=         4; j++) {
-          const input = document.getElementById(`modelo${i}-cor${j}`);
-          row.push(input.value || "0");
-        }
-        values.push(row);
+    for (let i = 1; i <= 24; i++) {
+      const row = [`Modelo ${i}`];
+      for (let j = 1; j <= 4; j++) {
+        const input = document.getElementById(`modelo${i}-cor${j}`);
+        row.push(input.value || "0");
       }
+      values.push(row);
+    }
 
-      const sheetsAPI = gapi.client.sheets.spreadsheets.values;
-      const accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
-
-      sheetsAPI
-        .append({
-          spreadsheetId: SPREADSHEET_ID,
-          range: "Página1",
-          valueInputOption: "RAW",
-          insertDataOption: "INSERT_ROWS",
-          resource: {
-            values: values,
-          },
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .then(
-          (response) => {
-            console.log("Resposta da API do Google Sheets:", response.result);
-            alert("Resposta enviada com sucesso!");
-          },
-          (error) => {
-            console.error(
-              "Erro ao enviar dados para o Google Sheets:",
-              error.result.error
-            );
-            alert("Erro ao enviar a resposta. Por favor, tente novamente.");
-          }
-        );
-    });
-}
+    const sheetsAPI = gapi.client.sheets.spreadsheets.values;
+    sheetsAPI
+      .append({
+        spreadsheetId: SPREADSHEET_ID,
+        range: "Página1",
+        valueInputOption: "RAW",
+        insertDataOption: "INSERT_ROWS",
+        resource: {
+          values: values,
+        },
+      })
+      .then(
+        (response) => {
+          console.log("Resposta da API do Google Sheets:", response.result);
+          alert("Resposta enviada com sucesso!");
+        },
+        (error) => {
+          console.error(
+            "Erro ao enviar dados para o Google Sheets:",
+            error.result.error
+          );
+          alert("Erro ao enviar a resposta. Por favor, tente novamente.");
+        }
+      );
+  });
 
